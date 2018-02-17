d19518f2a52ca809722fcfe422b09266b2fd809b
amymc <amy.mccarthy2@mail.dcu.ie>
2018-02-17 12:37:40 +0000

    initial commit

---
+++ background.js
@@ -0,0 +1,13 @@
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // No tabs or host permissions needed!
+//   debugger
+//   console.log('Turning ' + tab.url + ' red!')
+//   chrome.tabs.executeScript({
+//     code: 'document.body.style.backgroundColor="red"',
+//   })
+// })
+
+// "background": {
+//   "scripts": ["background.js"],
+//   "persistent": false
+// },
